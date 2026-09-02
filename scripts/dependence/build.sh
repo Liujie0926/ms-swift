8d56bc40ce31af1409d26c6ae6d05ba096cdc9e8
liujie44 <liujie44@baidu.com>
2026-09-02 14:20:39 +0800

    add workflow_dispatch

--- scripts/dependence/build.sh
+++ scripts/dependence/build.sh
@@ -26,8 +26,9 @@ python -m pip config --user set global.trusted-host pypi.tuna.tsinghua.edu.cn
 
 swift_tar (){
     cd /workspace
-    # ms-swift.tar only include the main branch
-    if [ -n "$BRANCH" ] && [ "$BRANCH" = "main" ]; then
+    if [ -n "$TARGET_COMMIT" ]; then
+        echo "TARGET_COMMIT=$TARGET_COMMIT specified, skip refreshing ms-swift.tar.gz"
+    elif [ -n "$BRANCH" ] && [ "$BRANCH" = "main" ]; then
         echo "Checkout branch $BRANCH"
         tar -zcf ms-swift.tar.gz ms-swift/
         mv ms-swift.tar.gz ${upload_path}/
@@ -60,11 +61,16 @@ swift_build (){
     new_name=$(echo $base_name | sed "s/\.dev0/&+${commit}/")
     echo "commit whl: $new_name"
     cp "$whl_file" "${upload_path}/${new_name}"
-    cp "$whl_file" "${upload_path}/${base_name}"
 
     zero_name=$(echo $base_name | sed "s/^ms_swift-[^-]*-/ms_swift-0.0.0-/")
-    echo "0.0.0 whl: $zero_name"
-    cp "$whl_file" "${upload_path}/${zero_name}"
+    if [ "${UPDATE_LATEST:-true}" = "true" ]; then
+        echo "latest whl: $base_name"
+        cp "$whl_file" "${upload_path}/${base_name}"
+        echo "0.0.0 whl: $zero_name"
+        cp "$whl_file" "${upload_path}/${zero_name}"
+    else
+        echo "UPDATE_LATEST=${UPDATE_LATEST}, skip latest whl: ${base_name} and 0.0.0 whl"
+    fi
 }
 
 # main
